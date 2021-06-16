8af41596397e60add004f732ff49c15bc7c24494
jinseong <jinseong205@gmail.com>
2021-06-16 13:52:01 +0900

    first

---
+++ DataTableTest.cs
@@ -0,0 +1,12 @@
+using System;
+namespace csPratice
+{
+    partial class DataTableTest
+    {
+        public DataTableTest()
+        {
+            Console.WriteLine("***DataTableClass init***");
+        }
+    }
+}
+ 
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace csPratice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataTableTest dtt = new DataTableTest();
+            
+        }
+        
+    }
+}
